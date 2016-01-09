94c09dc56120f05c9f7bc4abb7fd6d737820383b
Jason B. Cox <jcox1@linkedin.com>
2016-01-09 11:08:00 -0800

    Added rewrite-author script

---
+++ .scripts/rewrite-author.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="oldemail@domain.com"
+CORRECT_NAME="Jason B. Cox"
+CORRECT_EMAIL="contact@jasonbcox.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
