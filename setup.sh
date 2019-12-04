3d78a6f72a7ebe17cfd561dc58aa8de1891b0142
Jason B. Cox <contact@jasonbcox.com>
2019-12-03 16:42:42 -0800

    Add .gituser and .profile_alises to setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,22 @@
 
 set -euo pipefail
 
+TOPLEVEL=$(git rev-parse --show-toplevel)
+cd "$TOPLEVEL"
+
 mv .bashrc .bashrc.old || true
 mv .bash_profile .bash_profile.old || true
 
 ln -s .sourcemybashrc .bashrc
 ln -s .sourcemybashrc .bash_profile
-source .bashrc.custom
 
-mkdir -p ~/projects
+mkdir -p projects
+
+if [ ! -f .gituser ]; then
+  cp .templates/.gituser .
+  echo "Defaulting to this .gituser:"
+  cat .gituser
+fi
+
+# TODO: Copy .templates/.profile_aliases once it has been refactored to work on multiple platforms
+touch .profile_aliases
