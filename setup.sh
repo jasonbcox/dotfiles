ddb58b927bcfbe2d3c24fada64482b8f76547707
Jason B. Cox <contact@jasonbcox.com>
2019-12-20 16:54:42 -0800

    Add gpg config files

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 TOPLEVEL=$(git rev-parse --show-toplevel)
 cd "$TOPLEVEL"
 
+# Assume that .gnupg/gpg-agent.conf may have changed, so restart gpg-agent
+# (kill it, it will restart as needed)
+gpgconf --kill gpg-agent
+
 mv .bashrc .bashrc.old || true
 mv .bash_profile .bash_profile.old || true
 
