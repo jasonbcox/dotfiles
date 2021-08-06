562ccd448b16a5ae757a70935c216622fb101912
Jason B. Cox <contact@jasonbcox.com>
2021-08-06 14:21:58 -0700

    Add software-properties-common to install script

--- install-packages.sh
+++ install-packages.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install git vim tmux bash-completion dnsutils
+sudo apt-get install git vim tmux bash-completion dnsutils software-properties-common
