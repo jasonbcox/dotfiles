f13971ec73267f48521337cbc316d9a260ca9e06
Jason B. Cox <contact@jasonbcox.com>
2021-08-06 14:55:03 -0700

    Add gpgconf to install script

--- install-packages.sh
+++ install-packages.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install git vim tmux bash-completion dnsutils software-properties-common
+sudo apt-get install git gpgconf vim tmux bash-completion dnsutils software-properties-common
